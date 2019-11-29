ba2d1018f441cf8d2670d23f8aae13287931b0c6
Gracjan Szulc <gracjan.szulc2@ibm.com>
2019-11-29 02:27:16 +0100

    fixing dependecies and update on django settings

--- docker/nginx/app/djangodb/djangodb/settings.py
+++ docker/nginx/app/djangodb/djangodb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mysqlapp',
 ]
 
 MIDDLEWARE = [
@@ -79,7 +80,7 @@ DATABASES = {
     'NAME': 'employees',
     'USER': 'root',
     'PASSWORD': 'simplepassword',
-    'HOST': 'localhost', 
+    'HOST': 'db-mysql', 
   }
 }
 
